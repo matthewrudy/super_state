5afd6b00af01b31ea7cc11b98ba5760bb680d132
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2010-12-17 10:24:01 +0800

    failing test

---
+++ test/super_state_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class SuperStateTest < ActiveSupport::TestCase
+  
+  test "abstract the tests" do
+    puts "this has been abstracted from a live project"
+    puts "as such the tests have not yet been abstracted"
+    puts "give me some time to do this"
+    flunk "abstract the tests"
+  end
+  
+end
